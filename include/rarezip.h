10b2e591e2429f8cd1fc19509016e1a582eb6ed1
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-07-27 19:59:01 -0600

    Update rarezip.h

--- include/rarezip.h
+++ include/rarezip.h
@@ -1,66 +1,58 @@
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #ifndef RAREZIP_H
 #define RAREZIP_H
-#ifdef __cplusplus
-}
-#endif
-#include <ultra64.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+// Global decompression state
 extern struct huft *D_80007270;
+extern struct huft *D_80007290;
 
-extern u8 *inbuf; //inbuf
-extern u8 *D_80007284; //slide
-extern u32 inptr; //inptr
-extern u32 wp; //wp
-extern struct huft *D_80007290; //unk
-extern u32 bb; //bb
-extern u32 bk; //bk
-extern u32 crc1; //crc1
-extern u32 crc2; //crc2
-extern u32 hufts; //hufts
-
+extern u8 *inbuf;       // Input buffer (source data)
+extern u8 *D_80007284;  // Output buffer (decompressed data)
+extern u32 inptr;       // Current read position in inbuf
+extern u32 wp;          // Current write position in D_80007284
+extern u32 bb;          // Bit buffer
+extern u32 bk;          // Bit count
+extern u32 crc1;        // CRC1
+extern u32 crc2;        // CRC2
+extern u32 hufts;       // Huffman table usage tracker
+extern u32 g_decomp_out_cap; // Output buffer capacity
 
 #ifndef WSIZE
-#define WSIZE 0x8000     /* window size--must be a power of two, and */
-#endif                     /*  at least 32K for zip's deflate method */
+#define WSIZE 0x8000     /* window size--must be a power of two, and at least 32K for zip's deflate method */
+#endif
 
-//#define get_byte()  (inptr < insize ? inbuf[inptr++] : fill_inbuf(0))
-#define get_byte()  (inbuf[inptr++])
+#define get_byte() (inbuf[inptr++])
 
 #ifdef CRYPT
   uch cc;
-#define NEXTBYTE() \
-     (decrypt ? (cc = get_byte(), zdecode(cc), cc) : get_byte())
+#define NEXTBYTE() (decrypt ? (cc = get_byte(), zdecode(cc), cc) : get_byte())
 #else
-#define NEXTBYTE()  (u8)get_byte()
+#define NEXTBYTE() (u8)get_byte()
 #endif
-#define NEEDBITS(n) {while(k<(n)){b|=((u32)NEXTBYTE())<<k;k+=8;}}
-#define DUMPBITS(n) {b>>=(n);k-=(n);}
+
+#define NEEDBITS(n) { while(k < (n)) { b |= ((u32)NEXTBYTE()) << k; k += 8; } }
+#define DUMPBITS(n) { b >>= (n); k -= (n); }
 
 struct huft {
-  u8 e;                /* number of extra bits or operation */
-  u8 b;                /* number of bits in this code or subcode */
-  union {
-    u16 n;              /* literal, length base, or distance base */
-    struct huft *t;     /* pointer to next level of table */
-  } v;
+    u8 e;                /* number of extra bits or operation */
+    u8 b;                /* number of bits in this code or subcode */
+    union {
+        u16 n;          /* literal, length base, or distance base */
+        struct huft *t; /* pointer to next level of table */
+    } v;
 };
 
-/* If BMAX needs to be larger than 16, then h and x[] should be ulg. */
 #define BMAX 16         /* maximum bit length of any code (16 for explode) */
 #define N_MAX 288       /* maximum number of codes in any set */
 
-int bkboot_inflate(void);
-
-#endif
-
+// Renamed to match the wrapper in stubs.cpp
+int bkboot_inflate_unlocked(void);
 
 #ifdef __cplusplus
 }
 #endif
+
+#endif // RAREZIP_H
