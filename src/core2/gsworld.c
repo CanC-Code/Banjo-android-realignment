55cab049d4cd78cba52032b626e9a80d2b253275
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-08-01 18:41:15 -0600

    Add missing D_803835DC declaration

--- src/core2/gsworld.c
+++ src/core2/gsworld.c
@@ -11,6 +11,7 @@
 extern u8 D_80370250 = 0;
 
 extern struct { s32 unk0; s32 game_mode; f32 unk8; s32 unkC; f32 unk10; u8 transition; u8 map; u8 exit; u8 unk17; u8 unk18; u8 unk19; u8 unk1A; u8 unk1B; u8 unk1C; } D_8037E8E0;
+s32 D_803835DC;
 
 /* public */
 void func_80335110(s32);
